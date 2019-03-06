c1314ec18168b53bf6935d233cb3e5906939a65a
aryyya <29365565+aryyya@users.noreply.github.com>
2019-03-05 22:49:44 -0800

    Use generic class based views to get an enormous amount of free behaviour, courtesy of Django REST Framework.

--- paste/views.py
+++ paste/views.py
@@ -1,47 +1,12 @@
 from .models import Paste
 from .serializers import PasteSerializer
 
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import mixins
 from rest_framework import generics
 
-class PasteList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class PasteList(generics.ListCreateAPIView):
     queryset = Paste.objects.all()
     serializer_class = PasteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class PasteDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
+class PasteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Paste.objects.all()
     serializer_class = PasteSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
