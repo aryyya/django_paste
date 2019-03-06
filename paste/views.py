55d95973b48f3fc442078993618a3fe21f710a1a
aryyya <29365565+aryyya@users.noreply.github.com>
2019-03-05 19:19:48 -0800

    Use decorator functions.

--- paste/views.py
+++ paste/views.py
@@ -1,32 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Paste
 from .serializers import PasteSerializer
-from rest_framework.status import *
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def paste_list(request):
     """
-    List all code paste, or create a new paste.
+    List all code pastes, or create a new paste.
     """
     if request.method == 'GET':
         pastes = Paste.objects.all()
         serializer = PasteSerializer(pastes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PasteSerializer(data=data)
-        if (serializer.is_valid()):
+        serializer = PasteSerializer(data=request.data)
+        if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(serializer.errors, status=HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def paste_detail(request, pk):
     """
     Retrieve, update, or delete a code paste.
@@ -34,21 +35,20 @@ def paste_detail(request, pk):
     try:
         paste = Paste.objects.get(pk=pk)
     except Paste.DoesNotExist:
-        return HttpResponse(status=HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = PasteSerializer(paste)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PasteSerializer(paste, data=data)
+        serializer = PasteSerializer(paste, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, status=HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         paste.delete()
-        return HttpResponse(status=HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
