8626dfe940f1162f2293806b0a2712b42ca3fc51
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-07 12:55:56 -0800

    More readable formatting.

--- paste/urls.py
+++ paste/urls.py
@@ -5,12 +5,35 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.api_root),
-    path('pastes/', views.PasteList.as_view(), name='paste-list'),
-    path('pastes/<int:pk>/', views.PasteDetail.as_view(), name='paste-detail'),
-    path('pastes/<int:pk>/highlight/', views.PasteHighlight.as_view(), name='paste-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path(
+        '',
+        views.api_root
+    ),
+    path(
+        'pastes/',
+        views.PasteList.as_view(),
+        name='paste-list'
+    ),
+    path(
+        'pastes/<int:pk>/',
+        views.PasteDetail.as_view(),
+        name='paste-detail'
+    ),
+    path(
+        'pastes/<int:pk>/highlight/',
+        views.PasteHighlight.as_view(),
+        name='paste-highlight'
+    ),
+    path(
+        'users/',
+        views.UserList.as_view(),
+        name='user-list'
+    ),
+    path(
+        'users/<int:pk>/',
+        views.UserDetail.as_view(),
+        name='user-detail'
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
