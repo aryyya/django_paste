6ed009d9805d8a6f83eef31e342409d1f51d2b9e
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-07 13:38:13 -0800

    Add schema.

--- paste/urls.py
+++ paste/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from . import views
 
@@ -8,6 +9,9 @@ router = DefaultRouter()
 router.register(r'pastes', views.PasteViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Paste API')
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('schema/', schema_view)
 ]
